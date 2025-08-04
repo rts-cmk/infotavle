4b8dd3cb48dde5a60e2ed9752514c51bf2af6935
sasha <sasha.friis@icloud.com>
2025-08-04 13:45:04 +0200

    updated README

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,7 @@ import type { Route } from "./+types/home";
 export function meta({ }: Route.MetaArgs) {
   return [
     { title: "Infotavle" },
-    { name: "description", content: "Welcome to React Router!" },
+    { name: "description", content: "Roskilde tekniske skole infotavel" },
   ];
 }
 
