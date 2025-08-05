f6db5203e2e3d97ee204095b5a685c666b5bc92e
sasha <sasha.friis@icloud.com>
2025-08-05 09:34:03 +0200

    blank project

---
+++ infotavle/src/Home.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>home</h1>
+    </>
+  )
+}
+
+export default Home
