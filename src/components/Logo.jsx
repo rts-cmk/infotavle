5a55bcf36d8fd24a87ef8f77dcdab5345391f66e
Denisse Daugaard <U2478723@rts-365.dk>
2025-08-14 13:23:18 +0200

    resized logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -7,7 +7,7 @@ export default function Logo() {
       className="logo"
       src='/logo.svg'  alt="logo"// use imported variable
       initial={{ y: -600, scale: 0.5, rotate: 0, opacity: 0 }}
-      animate={{ y: -150, scale: 1, rotate: 360, opacity: 1}}
+      animate={{ y: -200, scale: 1, rotate: 360, opacity: 1}}
       transition={{ duration: 2, delay: 1, repeat:Infinity, repeatDelay: 50 }}
     />
   );
