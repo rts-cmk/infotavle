a1c6032fd8d7da6fa85d52e398b0c0548803e318
Denisse Daugaard <U2478723@rts-365.dk>
2025-08-14 12:01:08 +0200

    logo added

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -8,7 +8,7 @@ export default function Logo() {
       src='/logo.svg'  alt="logo"// use imported variable
       initial={{ y: -600, scale: 0.5, rotate: 0, opacity: 0 }}
       animate={{ y: -150, scale: 1, rotate: 360, opacity: 1}}
-      transition={{ duration: 2, delay: 1, repeat:Infinity, repeatDelay: 30 }}
+      transition={{ duration: 2, delay: 1, repeat:Infinity, repeatDelay: 50 }}
     />
   );
 }
