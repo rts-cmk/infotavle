6708bec1918d4b15188947c3916ca29d071ab2a2
trasha <sasha.friis@icloud.com>
2025-08-18 12:41:01 +0200

    deleted unnecessary code

--- src/components/InfoCarousel.jsx
+++ src/components/InfoCarousel.jsx
@@ -6,11 +6,10 @@ import Klasser from "./slides/Klasser";
 import Pauser from "./slides/Pauser";
 
 const InfoCarousel = () => {
-  const slides = [<Welcome />, <Klasser />, <Pauser />]
+  const slides = [<Welcome />, <Klasser />, <Pauser />];
   const [currentIndex, setCurrentIndex] = useState(0);
 
-
-   useEffect(() => {
+  useEffect(() => {
     if (slides.length === 0) return;
     const interval = setInterval(() => {
       setTimeout(() => {
@@ -21,18 +20,17 @@ const InfoCarousel = () => {
     return () => clearInterval(interval);
   }, [slides]);
 
-
   const currentSlide = slides[currentIndex];
 
   return (
     <AnimatePresence>
       <motion.div
-        // key={currentIndex}
-        // className="info__slider"
-        // initial={{ opacity: 0, }}
-        // animate={{ opacity: 1, x: 0 }}
-        // exit={{ opacity: 0 }}
-        // transition={{ duration: 2 }}
+      // key={currentIndex}
+      // className="info__slider"
+      // initial={{ opacity: 0, }}
+      // animate={{ opacity: 1, x: 0 }}
+      // exit={{ opacity: 0 }}
+      // transition={{ duration: 2 }}
       >
         {currentSlide}
       </motion.div>
@@ -42,20 +40,3 @@ const InfoCarousel = () => {
 
 export default InfoCarousel;
 
-
-
-
-//  useEffect(() => {
-//     fetch("/data/sampledata.json")
-//       .then((res) => res.json())
-//       .then((data) => {
-//         console.log(data); // check the data
-//         setSlides(data); // save data in state
-//       })
-//       .catch((err) => console.error("Error fetching data:", err));
-//   }, []);
-
-//   // If slides haven't loaded yet
-//   if (slides.length === 0) return <p>Loading...</p>;
-
-//   const currentSlide = slides[currentIndex];
