6883475542f554d8f8347f091109c08610bd958d
Denisse Daugaard <U2478723@rts-365.dk>
2025-08-18 12:38:50 +0200

    some slides added

---
+++ src/components/slides/slides__components/NavnCodes.js
@@ -0,0 +1,37 @@
+
+export const klasserNavn = {
+  1525: "Mediegrafiker",
+  1465: "Webudvikler",
+  1890: "Teknisk Designer",
+  '00329': "Grundforløb",
+
+};
+
+
+export const teachersNavn = {
+  be: "Brian",
+  tsj: "Torben",
+  dow: "Dorthe",
+  kbr: "Kristian",
+  shl: "Steen",
+  han: "Henrik",
+  ssh: "Steffen",
+  nf: "Nicolas",
+  miry: "Michael",
+  lase: "Lasse",
+  cns: "Claus",
+  mhp: "Michael",
+  henj: "Henrik",
+  aij: "Anders",
+  mfbp: "Morten",
+  sta: "Lone",
+  klba: "Klavs",
+  nas: "Naseem",
+  kala: "Karsten",
+  pdh: "Pernille",
+  mabj: "Martin",
+  lac: "Lasse",
+  joro: "Jon",
+  olp: "Ole",
+  clu: "Carsten",
+};
